bd0b8283e371c377354b32fdeba030cfc0870c4d
luizlibardi <luizlibardi@hotmail.com>
2021-09-22 15:52:31 -0300

    Primeiro contato com a Linguagem

---
+++ basico.js
@@ -0,0 +1,42 @@
+console.log('ola')
+
+// variaveis (var, let e const)
+// var a = 1
+// var b = 2
+// const c = a + b 
+
+// console.log(c)
+
+// operadores
+// unarios
+// let a = 3
+// a++ //postfix
+// ++a //prefix
+// console.log(a)
+
+// binarios (aritimeticos, atribuicao, relacionais, logicos)
+
+// let b = 557
+// let x = 2 + 3 // infix
+
+// let a = 3 
+// a += 3
+// a = a + 3
+// a *= 7
+// console.log(a)
+
+let temSol = false
+let estouComDinheiro = false
+let vouSair = temSol && estouComDinheiro
+// console.log(vouSair ? 'Ebaaa!' : 'Que chato!!!')
+let resultado = vouSair ? 'Ebaaa!' : 'Que chato!!!'
+console.log(resultado)
+
+// ternario
+
+//estruturas de controle
+
+// funcao
+
+// Objeto
+
