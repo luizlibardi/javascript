14e7fc7556f63eb7f90c8a4d0359912a18170efb
luizlibardi <luizlibardi@hotmail.com>
2021-10-11 11:10:53 -0300

    tipos de dados

---
+++ tipos-de-dados.js
@@ -0,0 +1,33 @@
+// Números
+10;
+10.5;
+
+// Strings
+"Olá";
+'Olá';
+
+// booleanos
+true;
+false;
+
+// arrays
+
+[10, "oi", false];
+
+// Objetos
+
+{
+    nome: 'João'
+    sobrenome: 'das neves'
+};
+
+// Tipagem diâmica
+
+var y = 15;
+y = "olá";
+
+// Outros valores
+
+var z; // undefined
+
+var pessoa = null; 
