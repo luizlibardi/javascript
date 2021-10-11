93226479cf0e774f17aa3a8055414ccb5fdbb022
luizlibardi <luizlibardi@hotmail.com>
2021-10-11 15:14:50 -0300

    utilizando prototypes em javascript

---
+++ prototype.js
@@ -0,0 +1,29 @@
+function Stark(nome, idade, corDoCabelo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.corDoCabelo = corDoCabelo;
+
+    this.sobrenome = "Stark";
+    this.apresentar = function (){
+        console.log("Olá, eu sou "+ this.nome + ' ' + this.sobrenome + '.');
+    }
+    
+}
+
+var ned = new Stark("Ned", 40, "Preto");
+Stark.prototype.darTchau = function() { console.log("Tchau"); };
+ned.apresentar();
+var sansa = new Stark("Sansa", 13, "Cobre");
+sansa.apresentar();
+sansa.darTchau();
+console.log(ned);
+console.log(sansa);
+
+
+String.prototype.apagar = function() {
+    return "";
+
+}
+
+console.log("oi".apagar());
+
