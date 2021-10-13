645fd5fe9919816b69f2db2220d2bf8a65e78810
luizlibardi <luizlibardi@hotmail.com>
2021-10-13 09:31:52 -0300

    manipulando arrays

--- arrays.js
+++ arrays.js
@@ -2,3 +2,42 @@
 
 var stark = ["Ned", "Jon", "Rob"];
 console.log(stark[1])
+
+var array = "Ned Jon Robb Bran Rickon".split(' ');
+
+console.log(array.toString());
+console.log(array.join(' | '));
+
+// Insere o Ultimo elemento
+array.push("Sansa");
+
+// Insere o Primeiro elemento
+array.unshift("Sansa");
+
+console.log(array);
+
+// Remove o Ultimo elemento
+var elemento = array.pop();
+elemento = array.shift();
+
+console.log(elemento);
+console.log(array);
+
+array[1] = "Jon Snow";
+
+console.log(array);
+
+// Não altera o Array original
+var slice = array.slice(3, 4);
+console.log(slice);
+
+// Altera o Array original
+var splice = array.splice(1, 1, "Arya", "Sansa")
+console.log(splice);
+console.log(array);
+
+array.splice(3, 1);
+console.log(array);
+
+array = array.concat(splice, slice);
+console.log(array);
